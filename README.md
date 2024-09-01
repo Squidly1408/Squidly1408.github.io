bcbe027ad8475ff07377201b28a5fe5734170471
Lucas Newman <Lucas.newman140@gmail.com>
2024-09-01 17:57:04 +1000

    added quotes to portfolio

--- README.md
+++ README.md
@@ -7,9 +7,9 @@
 This is the test for a new version of my portfolio. It is based on feedback from many software engineers, and inspired by others. it has 4 segments.
 
 1: Introduction to the website / banner \
-2: About me / main skills I know \
-3: Four of my best projects \
-4: four of my best Internships / job oppotunites
+2: About me / My skills / Big Picture Education Requirments \
+3: My best Projects \
+4: Experience
 
 
 ## 🛠 Skills used
--- script.js
+++ script.js
@@ -10,3 +10,39 @@ function Scroll(Element){
 function back() {
   window.location.href="../index.html";
 }
+
+const quotes = [
+  '"Lucas is a rare kind of student, motivated, innovative, and determined to make a positive impact on the world around him. It is a rare joy to find a student with such focus and drive, I look forward to seeing his education and career prosper."',
+  '"Lucas was an absolute pleasure to deal with. He assisted us with some website and graphic design tasks. He performed them to a very high standard. \n\n I have no hesitation in recommending Lucas to any local design or development companies."',
+  '“Lucas is a talented future engineer with a bright future ahead. He has a passion for technology and it shows!”',
+]
+
+const authors = [
+  'Dr. Jacqueline D Bailey, Associate Lecturer - Computing & IT, The University of Newcastle',
+  'Chris, Director of CLICKK',
+  'Liam, CCO of SAPHI',
+]
+
+let currentQuote = 0;
+let currentAuthor = 0;
+const switchTime = 8000;
+
+// Auto switch text every switchTime milliseconds
+setInterval(showNext, switchTime);
+
+function showNext() {
+  currentQuote = (currentQuote + 1) % quotes.length;
+  currentAuthor = (currentAuthor + 1) % authors.length;
+  updateText();
+}
+
+function showPrev() {
+  currentQuote = (currentQuote - 1 + quotes.length) % quotes.length;
+  currentAuthor = (currentAuthor - 1 + authors.length) % authors.length;
+  updateText();
+}
+
+function updateText() {
+  document.getElementById('quote').innerText = quotes[currentQuote];
+  document.getElementById('author').innerText = authors[currentAuthor];
+}
