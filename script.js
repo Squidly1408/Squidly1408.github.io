3ee9bdcfaccb36185ee8ad5626d2c6360e05c2b4
Lucas Newman <Lucas.newman140@gmail.com>
2024-09-14 11:19:58 +1000

    adding skills to projects and experience

--- script.js
+++ script.js
@@ -15,17 +15,19 @@ const quotes = [
   '"Lucas is a rare kind of student, motivated, innovative, and determined to make a positive impact on the world around him. It is a rare joy to find a student with such focus and drive, I look forward to seeing his education and career prosper."',
   '"Lucas was an absolute pleasure to deal with. He assisted us with some website and graphic design tasks. He performed them to a very high standard. \n\n I have no hesitation in recommending Lucas to any local design or development companies."',
   '“Lucas is a talented future engineer with a bright future ahead. He has a passion for technology and it shows!”',
+  '"As Lucas’s teacher when he began his journey in project-based learning, I had the pleasure of seeing him demonstrate incredible adaptability, a deep thirst for knowledge, and a keen ability to solve problems. What stood out most was his consistent incorporation of social justice into your projects, showcasing not only his intellect but also his commitment to making a positive impact in the world."'
 ]
 
 const authors = [
   'Dr. Jacqueline D Bailey, Associate Lecturer - Computing & IT, The University of Newcastle',
   'Chris, Director of CLICKK',
-  'Liam, CCO of SAPHI',
+  'Liam, CCO of SAPHI Engineering',
+  'Thomas Deane, Head Teacher of Well Being, Newcastle High School',
 ]
 
 let currentQuote = 0;
 let currentAuthor = 0;
-const switchTime = 8000;
+const switchTime = 15000;
 
 // Auto switch text every switchTime milliseconds
 setInterval(showNext, switchTime);
